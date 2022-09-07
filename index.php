4afe0801b5511e5baa7812ede26cce1c31283e5e
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-09-07 14:22:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php require_once("header.php"); ?>
-<h1>My Website</h1>
+<h5>Nick Pranske's Website</h5>
+  <form action="welcome.php" method="post">
+      Name: <input type="text" name="name"><br>
+      E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+  </form>
 <?php require_once("footer.php"); ?>
