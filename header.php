89d9737a28ab96b8ead03b9d199ae9cb04e5e134
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-09-18 16:40:07 -0500

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   <head>
     <style>
       div {text-align: center;}
+    <style/>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
