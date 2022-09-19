74529f361f18a2830bbd5e050aa533b08c30a316
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-09-18 21:45:34 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,6 +1,11 @@
 <?php require_once("header.php"); ?>
-
+<br><br/>
+<h5>
+  Thank you for your submission.
+<h5/>
+<br><br/><br><br/>
+<div style="text-align:center; margin:auto;">
   Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
-
+<div/>
 <?php require_once("footer.php"); ?>
