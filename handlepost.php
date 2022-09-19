fee3c42105d2ff93ed0ad3ce3cccbe80b39e77a0
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-09-18 21:22:49 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,6 +1,6 @@
 <?php require_once("header.php"); ?>
 
-  Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+  Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
 
 <?php require_once("footer.php"); ?>
