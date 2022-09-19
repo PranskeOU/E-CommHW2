53e1267867d2cc6b3b70383c5374f3aed0da8590
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-09-18 21:42:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php require_once("header.php"); ?>
-<br><br/><br><br/><br><br/>
+<br><br/>
+<h5>HTTP POST Form Submit:<h5/>
+<br><br/><br><br/>
 <div style="text-align:center; margin:auto">
   <form action="handlepost.php" method="get">
       Name: <input type="text" name="name"><br>
