c00f08c410a86ef63dc1f967b9d418bd160c88b2
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-09-26 14:17:24 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,11 +1,14 @@
 <?php require_once("header.php"); ?>
 <br><br/>
 <h5 style="text-align:center;">
-  <?php ThankYouMessage(); ?>
+  Thank you for your submission.
 <h5/>
 <br><br/><br><br/>
 <div style="text-align:center; margin:auto;">
   Welcome, <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
+<div/>
+<div style="text-align:center; margin:auto;">
+  <?php ThankYouMessage(); ?>
 <div/>
 <?php require_once("footer.php"); ?>
