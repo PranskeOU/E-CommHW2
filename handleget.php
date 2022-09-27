d245c71f4b4cf040afca7585cf17c6fb57bb1130
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-09-27 17:04:56 -0500

    Create handleget.php

---
+++ handleget.php
@@ -0,0 +1,12 @@
+<?php require_once("header.php"); ?>
+<?php require_once("ThankYouMessage.php"); ?>
+<br><br/>
+<h5 style="text-align:center;">
+  <?php ThankYouMessage(); ?>
+<h5/>
+<br><br/><br><br/>
+<div style="text-align:center; margin:auto;">
+  Welcome, <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+<div/>
+<?php require_once("footer.php"); ?>
